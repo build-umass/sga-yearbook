826e37df859e38f53041175b1b3267e1e96805e7
Timothy Nguyen <quynhthoa1972@gmail.com>
2021-10-28 08:37:14 -0400

    Fix some issues with images. Bugs that may need to look into the future

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -4,7 +4,9 @@ import Head from 'next/head'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import Image from 'next/image'
+import Image from 'react-bootstrap/Image'
+import brand from '../images/indexLogoInline.png';
+// import Img from 'react-optimized-image';
 
 export default function Index() {
     return (
@@ -15,7 +17,7 @@ export default function Index() {
             <Navbar bg="transparent" expand="lg">
                 <Container>
                     <Navbar.Brand href="/">
-                        <Image src='/indexLogoInline.png' alt="site logo" width={400} height={50} />
+                        <Image src={brand} alt="site logo" width={400} height={50} />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse
